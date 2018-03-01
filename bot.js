41810a77d227dc1d327f7c67f5fd18f59fcac5f2
SouL03 <34333991+SouL03@users.noreply.github.com>
2018-03-01 23:17:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,25 +15,25 @@ client.on("message", (message) => {
   if (message.content === prefix + 'silver') {
     message.delete();
     message.author.sendMessage('Ciao ' + message.author + '. Ecco il logo della Silver Accademy');
-    message.author.sendFile("https://imgur.com/BIZfrfR");
+    message.author.sendFile("./Silver-Accademy.png");
   }
 
   if (message.content === prefix + 'gold') {
     message.delete();
     message.author.sendMessage('Ciao ' + message.author + '. Ecco il logo della Gold Accademy');
-    message.author.sendFile("https://imgur.com/Na9fD8g");
+    message.author.sendFile("./Gold-Accademy.png");
   }
 
   if (message.content === prefix + 'platinum') {
     message.delete();
     message.author.sendMessage('Ciao ' + message.author + '. Ecco il logo della Platinum Accademy');
-    message.author.sendFile("https://imgur.com/vMDx2ok");
+    message.author.sendFile("./Platinum-Accademy.png");
   }
 
   if (message.content === prefix + 'diamond') {
     message.delete();
     message.author.sendMessage('Ciao ' + message.author + '. Ecco il logo della Diamond Accademy');
-    message.author.sendFile("https://imgur.com/bCiLkXv");
+    message.author.sendFile("./Diamond-Accademy.png");
   }
 
   if (message.content === prefix + 'champion') {
@@ -45,7 +45,7 @@ client.on("message", (message) => {
   if (message.content === prefix + 'gc') {
     message.delete();
     message.author.sendMessage('Ciao ' + message.author + '. Ecco il logo della GrandChampion Accademy');
-    message.author.sendFile("https://imgur.com/ZtZ58ko");
+    message.author.sendFile("./GrandChampion-Accademy.png");
   }
 
 });
