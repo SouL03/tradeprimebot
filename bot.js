a3cb896788e8301efd7463c24b00d2acfaa21dc3
SouL03 <34333991+SouL03@users.noreply.github.com>
2018-03-01 23:04:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = "!";
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on("message", (message) => {
-
-  const prefix = "!";
 
   if (message.content === prefix + 'silver') {
     message.delete();
