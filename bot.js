618e878345cc65a0cd91c771c02c220bfb9e50e1
SouL03 <34333991+SouL03@users.noreply.github.com>
2018-03-01 23:01:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,16 +5,45 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
-  	}
+bot.on("ready", () => {
+  console.log('Sono pronto per essere utilizzato!')
 });
 
-client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
-  	}
+bot.on("message", (message) => {
+
+  const prefix = botsettings.prefix
+
+  if (message.content === prefix + 'silver') {
+    message.delete();
+    message.author.sendMessage('Ciao ' + message.author + '. Ho visto che vorresti la foto profilo della Silver Accademy. Ti arriverà fra qualche secondo! ;)');
+    message.author.sendFile("https://imgur.com/BIZfrfR");
+  }
+
+  if (message.content === prefix + 'gold') {
+    message.delete();
+    message.author.sendMessage('Ciao ' + message.author + '. Ho visto che vorresti la foto profilo della Gold Accademy. Ti arriverà fra qualche secondo! ;)');
+    message.author.sendFile("https://imgur.com/Na9fD8g");
+  }
+
+  if (message.content === prefix + 'platino') {
+    message.delete();
+    message.author.sendMessage('Ciao ' + message.author + '. Ho visto che vorresti la foto profilo della Platinum Accademy. Ti arriverà fra qualche secondo! ;)');
+    message.author.sendFile("https://imgur.com/vMDx2ok");
+  }
+
+  if (message.content === prefix + 'diamond') {
+    message.delete();
+    message.author.sendMessage('Ciao ' + message.author + '. Ho visto che vorresti la foto profilo della Diamond Accademy. Ti arriverà fra qualche secondo! ;)');
+    message.author.sendFile("https://imgur.com/bCiLkXv");
+  }
+
+  if (message.content === prefix + 'champion') {
+    message.delete();
+    message.author.sendMessage('Ciao ' + message.author + '. Ho visto che vorresti la foto profilo della Champion Accademy. Ti arriverà fra qualche secondo! ;)');
+    message.author.sendFile("https://imgur.com/ZtZ58ko");
+  }
+
+bot.user.setGame("Animus eSports");
 });
 
 // THIS  MUST  BE  THIS  WAY
