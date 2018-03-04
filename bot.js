d5f52983b9ec0050314809356148b1ec72643828
SouL03 <34333991+SouL03@users.noreply.github.com>
2018-03-04 04:27:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,21 +7,12 @@ client.on("ready", () => {
     client.user.setStatus('online', 'Call of Duty: Black Ops 10');
 });
 
-      if (message.channel.type == 'text') {
-        message.channel.fetchMessages()
-          .then(messages => {
-            message.channel.bulkDelete(messages);
-            messagesDeleted = messages.array().length; // number of messages deleted
-
-            // Logging the number of messages deleted on both the channel and console.
-            message.channel.sendMessage("Deletion of messages successful. Total messages deleted: "+messagesDeleted);
-            console.log('Deletion of messages successful. Total messages deleted: '+messagesDeleted)
-          })
-          .catch(err => {
-            console.log('Error while doing Bulk Delete');
-            console.log(err);
-          });
-      }
+client.on('message', messaage => {
+    if(message.content.startsWith("!cosa")) {
+     message.delete(1000);
+     message.channel.send(message.content.slice{5, message.content.length});
+    }
+})
 
 });
 
