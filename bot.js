d3dc5783261f2be8840f3b5632b8946c05a8c190
SouL03 <34333991+SouL03@users.noreply.github.com>
2018-03-04 04:18:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,13 +9,18 @@ client.on("ready", () => {
 
 client.on('message', function(message) {
     if (message.content == "^clear") {
-        if (message.member.hasPermission("MANAGE_MESSAGES")) {
-            message.channel.fetchMessages()
-               .then(function(list){
-                    message.channel.bulkDelete(list);
-                }, function(err){message.channel.send("ERROR: ERROR CLEARING CHANNEL.")})                        
+        try {
+            if (message.member.hasPermission("MANAGE_MESSAGES")) {
+                messages = message.channel.fetchMessages();
+                message.channel.bulkDelete(messages);
+            }
+        } catch(e) {
+            message.channel.send("ERROR: ERROR CLEARING CHANNEL.");
+            console.log(e);
         }
     }
+
+});
 
 });
 
