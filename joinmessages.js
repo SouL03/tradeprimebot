dcbe92b39f0cc6fd7fb144ffd7f3c5cbd7fdf4f5
SouL03 <34333991+SouL03@users.noreply.github.com>
2018-03-13 03:54:56 +0100

    Delete joinmessages.js

--- joinmessages.js
+++
@@ -1,7 +0,0 @@
-const Discord = require('discord.js');
-const client = new Discord.Client ();
-
-client.on('guildMemberAdd', member => {
-       member.guild.defaultChannel.send(`Benvenuto nel server, ${member}!`);
-       console.log(`${member.user.username} è entrato`);
-});
