165b33f160a783261aa985e48c1472e618ed4571
SouL03 <34333991+SouL03@users.noreply.github.com>
2018-03-13 04:15:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ client.on("ready", () => {
 client.on("message", (message) => {    
   
   //Montage Command
-  if (message.content === prefix + 'montages') {
+  if (message.content === prefix + 'giochi') {
     message.delete(10000);
     message.channel.send({embed: {
         color: 3447003,
@@ -22,11 +22,11 @@ client.on("message", (message) => {
           name: client.user.username,
           icon_url: client.user.avatarURL
         },
-        title: "Animus eSports Montages",
-        description: "La lista dei montages caricati sul canale YouTube",
+        title: "AmazingPlays Gaming - Giochi",
+        description: "La lista dei giochi supportati sul nostro server discord",
         fields: [{
-            name: "ANIMUS ESPORTS | BEST GOALS AND SAVES | MONTAGE",
-            value: "https://www.youtube.com/playlist?list=PLxfysA9PEn_vBykbYZm6f5vFLFTHKidMP - Animus eSports YouTube Playlist"
+            name: "Giochi",
+            value: "- **Dead by Daylight**\- **Rocket League**\- **Deceit**\- **PUBG**\- **Fornite**"
           }
         ],
       }
@@ -35,51 +35,6 @@ client.on("message", (message) => {
   }
     
     
-    
-  //Accademy Commands
-    
-  if (message.content === prefix + 'silver') {
-    message.delete();
-    message.author.sendMessage('Ciao ' + message.author + '. Ecco il logo della Silver Accademy');
-    message.author.sendFile("./Silver-Accademy.png");
-  }
-
-  if (message.content === prefix + 'gold') {
-    message.delete();
-    message.author.sendMessage('Ciao ' + message.author + '. Ecco il logo della Gold Accademy');
-    message.author.sendFile("./Gold-Accademy.png");
-  }
-
-  if (message.content === prefix + 'platinum') {
-    message.delete();
-    message.author.sendMessage('Ciao ' + message.author + '. Ecco il logo della Platinum Accademy');
-    message.author.sendFile("./Platinum-Accademy.png");
-  }
-
-  if (message.content === prefix + 'diamond') {
-    message.delete();
-    message.author.sendMessage('Ciao ' + message.author + '. Ecco il logo della Diamond Accademy');
-    message.author.sendFile("./Diamond-Accademy.png");
-  }
-
-  if (message.content === prefix + 'champion') {
-    message.delete();
-    message.author.sendMessage('Ciao ' + message.author + '. Ecco il logo della Champion Accademy');
-    message.author.sendFile("./Champion-Accademy.png");
-  }
-    
-  if (message.content === prefix + 'gc') {
-    message.delete();
-    message.author.sendMessage('Ciao ' + message.author + '. Ecco il logo della GrandChampion Accademy');
-    message.author.sendFile("./GrandChampion-Accademy.png");
-  }
-    
-  //Who command
-  
-  if (message.content === prefix + 'am') {
-   message.delete(7000);
-   message.channel.send('L\'Animus eSports è un clan di Rocket League, questo è il server discord. Qui si puo\' diventare mebri di questo gruppo, conoscere nuovi giocatori, creare squadre etc... il nostro clan organizza tornei, sempre di Rocket League, dove si consolideranno i rapporti tra i vari team, e tra noi membri del clan. Animus eSports ha anche un canale youtube dove caricheremo soprattutto montaggi, con i migliori goal dei membri di questo gruppo:  https://www.youtube.com/channel/UCzj_OqA5BIlXeX1thOtQmGA?view_as=subscriber').then(msg => msg.delete(120000));   
-  }
     
   //Help Command
     
@@ -94,16 +49,8 @@ client.on("message", (message) => {
         title: "Comandi Supportati",
         description: "La lista dei comandi supportati del bot Animus eSports",
         fields: [{
-            name: "Comandi Accademie",
-            value: "__!silver__ = Immagine della **Silver Accademy**\n__!gold__ = Immagine della **Gold Accademy**\n__!platinum__ = Immagine della **Platinum Accademy**\n__!diamond__ = Immagine della **Diamond Accademy**\n__!champion__ = Immagine della **Champion Accademy**\n__!gc__ = Immagine della **GrandChampion Accademy**"
-          },
-                 {
-            name: "Comando AM",
-            value: "__!am__ = Descrizione degli **Animus eSports**"
-          },
-                 {
-            name: "Comando MONTAGES",
-            value: "__!montages__ = Lista dei Montages degli **Animus eSports**"
+            name: "Comando Giochi",
+            value: "__*!giochi*__ = Lista dei **giochi supportati**"
           }
         ],
       }
