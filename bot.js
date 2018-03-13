88820be18efe3d4ac0560262d90d058b1033b62f
SouL03 <34333991+SouL03@users.noreply.github.com>
2018-03-13 04:03:12 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ const prefix = "!";
 client.on("ready", () => {
     console.log("I am ready!");
     client.user.setGame("AmazingPlays");
+    client.user.setPresence({ game: { name: 'AmazingPlays', type: 0 } });
 });
 
 
