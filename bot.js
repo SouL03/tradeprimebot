ad66eee4a3222dd03817d2cf71ef7cd9089ee488
SouL03 <34333991+SouL03@users.noreply.github.com>
2018-03-14 18:45:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,40 +14,38 @@ client.on("ready", () => {
 //Bot Custom Commands
 client.on("message", (message) => {
     
-    if (message.content == CLEAR_MESSAGES) {
+  let deleteStuff = () => {
+    let count = 0;
+    msg.channel.fetchMessages({limit: 100})
+     .then(messages => {
+       let messagesArr = messages.array();
+       let messageCount = messagesArr.length;
 
-      // Check the following permissions before deleting messages:
-      //    1. Check if the user has enough permissions
-      //    2. Check if I have the permission to execute the command
-
-      if (!message.channel.permissionsFor(message.author).hasPermission("MANAGE_MESSAGES")) {
-        message.channel.sendMessage("Sorry, you don't have the permission to execute the command \""+message.content+"\"");
-        console.log("Sorry, you don't have the permission to execute the command \""+message.content+"\"");
-        return;
-      } else if (!message.channel.permissionsFor(bot.user).hasPermission("MANAGE_MESSAGES")) {
-        message.channel.sendMessage("Sorry, I don't have the permission to execute the command \""+message.content+"\"");
-        console.log("Sorry, I don't have the permission to execute the command \""+message.content+"\"");
-        return;
-      }
-
-      // Only delete messages if the channel type is TextChannel
-      // DO NOT delete messages in DM Channel or Group DM Channel
-      if (message.channel.type == 'text') {
-        message.channel.fetchMessages()
-          .then(messages => {
-            message.channel.bulkDelete(messages);
-            messagesDeleted = messages.array().length; // number of messages deleted
-
-            // Logging the number of messages deleted on both the channel and console.
-            message.channel.sendMessage("Deletion of messages successful. Total messages deleted: "+messagesDeleted);
-            console.log('Deletion of messages successful. Total messages deleted: '+messagesDeleted)
+       for(let i = 0; i < messageCount; i++) {
+         messagesArr[i].delete()
+          .then(function() {
+            count = count + 1;
+            if(count >= 100) {
+              deleteStuff();
+            }
           })
-          .catch(err => {
-            console.log('Error while doing Bulk Delete');
-            console.log(err);
-          });
-      }
-    }
+          .catch(function() {
+            count = count + 1;
+            if(count >= 100) {
+              deleteStuff();
+            }
+          })
+       }
+     })
+     .catch(function(err) {
+       console.log('error thrown');
+       console.log(err);
+     });
+  };
+  
+  if(messageContent === '!:cleanse') {
+    deleteStuff();
+  }
   
   //Montage Command
   if (message.content === prefix + 'giochi') {
