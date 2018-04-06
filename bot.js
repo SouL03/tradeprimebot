fc3ac68a51ea4896f8f821f9926b3d375329d2ef
SouL03 <34333991+SouL03@users.noreply.github.com>
2018-04-06 05:01:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,12 +9,24 @@ client.on("ready", () => {
     client.user.setPresence({ game: { name: 'AmazingPlays Gaming 📘', type: 0 } });
 });
 
-
-
-//Bot Custom Commands
-client.on("message", (message) => {
-    
-      if(command === "purge") {
+client.on("message", async message => {
+  // This event will run on every single message received, from any channel or DM.
+  
+  // It's good practice to ignore other bots. This also makes your bot ignore itself
+  // and not get into a spam loop (we call that "botception").
+  if(message.author.bot) return;
+  
+  // Also good practice to ignore any message that does not start with our prefix, 
+  // which is set in the configuration file.
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  
+  // Here we separate our "command" name, and our "arguments" for the command. 
+  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+  // command = say
+  // args = ["Is", "this", "the", "real", "life?"]
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  if(command === "purge") {
     // This command removes all messages from all users in the channel, up to 100.
     
     // get the delete count, as an actual number.
@@ -29,6 +41,10 @@ client.on("message", (message) => {
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
+});
+
+//Bot Custom Commands
+client.on("message", (message) => {
     
       if (message.content == CLEAR_MESSAGES) {
 
