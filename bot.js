d6adaa8264f11110fd97858d38d28e2cd4fb1358
SouL03 <34333991+SouL03@users.noreply.github.com>
2018-04-07 23:16:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,9 +14,9 @@ client.on("ready", () => {
 //Bot Custom Commands
 client.on("message", (message) => {
     
-      if (message.author.bot || !message.content.startsWith(cfg.prefix)) {
-          message.channel.sendMessage('OK');
-      }
+      //if (message.author.bot || !message.content.startsWith(cfg.prefix)) {
+          //message.channel.sendMessage('OK');
+      //}
       //if (message.author.client || !message.content.startsWith(cfg.prefix)) return;
         //message.channel.send('OK');
       
