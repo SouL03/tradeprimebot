feeb38830371d030a12246b54910ac4bf89b9735
SouL03 <34333991+SouL03@users.noreply.github.com>
2018-04-08 02:46:20 +0200

    Update await.js

--- await.js
+++ await.js
@@ -1,6 +1,14 @@
+const agree = "✅";
+const disagree = "❎";
+
 module.exports.run = async (bot, message, args) => {
-    const reactions = await message.awaitReactions(reaction => reaction.name === "white_check_mark", {time: 7000});
-    console.log(reactions);
+    
+    let msg = await message.channel.send("Vota!");
+    await msg.react(agree);
+    await msg.react(disagree);
+    
+    const reactions = await message.awaitReactions(reaction => reaction.emoji.name === agree || reaction.emoji.name === disagree, {time = 15000});
+    message.channel.send('Voti completati! \n\n${agree}: ${reactions.get(agree).count=1}\n$(disagree): {reactions.get(disagree).count=1}');
 }
 
 module.exports.help = {
