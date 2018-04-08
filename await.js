e9ac5ea70018640668dbe91a7438a4792dc41b39
SouL03 <34333991+SouL03@users.noreply.github.com>
2018-04-08 02:36:34 +0200

    Update await.js

--- await.js
+++ await.js
@@ -1,5 +1,6 @@
 module.exports.run = async (bot, message, args) => {
-  
+    const reactions = await message.awaitReactions(reaction => reaction.name === "white_check_mark", {time: 7000});
+    console.log(reactions);
 }
 
 module.exports.help = {
