8a891c592065f278bd3d1e46e85b0f6a5d816877
SouL03 <34333991+SouL03@users.noreply.github.com>
2018-04-09 13:43:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,20 +12,21 @@ client.on("ready", () => {
 client.on("message", async message => {
        const args = message.content.slice(cfg.prefix.length).trim().split(/ +/g);
        const command = args.shift().toLowerCase();
-    
+    if(command === 'fai') {
        const user = message.mentions.users.first();
        const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
        if (!amount) return message.reply('Must specify an amount to delete!');
        if (!amount && !user) return message.reply('Must specify a user and amount, or just an amount, of messages to purge!');
        message.channel.fetchMessages({
-       limit: amount,
+         limit: amount,
        }).then((messages) => {
-       if (user) {
-          const filterBy = user ? user.id : Client.user.id;
-          messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
-       }
-       message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
+         if (user) {
+         const filterBy = user ? user.id : Client.user.id;
+         messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
+         }
+         message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
        });
+    }
     
        if(message.content === cfg.prefix + 'elimina') {
          let numberMessages = args[0];
