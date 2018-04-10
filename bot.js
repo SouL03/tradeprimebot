7716fc3ad26aa2d86054a9bbd42f2bf645a0ea42
SouL03 <34333991+SouL03@users.noreply.github.com>
2018-04-10 04:03:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ client.on("message", async message => {
 
 client.on('guildMemberAdd', member => {
        //member.guild.defaultChannel.send(`Benvenuto nel server, ${member}!`);
-       member.guild.author.sendMessage('We strunz');
+       member.guild.author.send('We strunz');
        console.log(`${member.user.username} has joined`);
 });
 
