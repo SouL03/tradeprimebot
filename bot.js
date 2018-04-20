79bc33b5572a2ffd603e8f29fcef0b4ef544619a
SouL03 <34333991+SouL03@users.noreply.github.com>
2018-04-20 18:12:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,17 +8,13 @@ client.on("ready", () => {
 
 client.on('messageReactionAdd', (reaction, user) => {
     const msg = reaction.message;
-        //if(reaction.emoji.name === client.emojis.get("431581850457079819")) {
-        
-        //Funzionale => reaction.emoji === client.emojis.get("436916081957797889"
-        
-        
-        if (reaction.emoji.name === "✅") {
-            const Steam = reaction.message.guild.roles.find('name','Steam')
-            var realuser = reaction.message.guild.member(user)
-            //user.addRole(user.guild.roles.add("name", "Steam"));
-            realuser.addRole(Steam);
-        }
+    //if(reaction.emoji.name === client.emojis.get("431581850457079819")) {
+    if (reaction.emoji === client.emojis.get("431581850457079819")) {
+        const Steam = msg.guild.roles.find('name','Steam')
+        var realuser = reaction.message.guild.member(user)
+        //user.addRole(user.guild.roles.add("name", "Steam"));
+        realuser.addRole(Steam);
+    }
 });
 
 client.on("message", async message => {
