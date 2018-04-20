0e8dfe5454fb4528ebf7959ff4b704b6a260e06d
SouL03 <34333991+SouL03@users.noreply.github.com>
2018-04-20 18:37:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,13 +8,15 @@ client.on("ready", () => {
 
 client.on('messageReactionAdd', (reaction, user) => {
     const msg = reaction.message;
-    //if(reaction.emoji.name === client.emojis.get("431581850457079819")) {
-    //client.emojis.get("436916081957797889")
-    if (reaction.emoji.id === reaction.message.guild.emojis.find("name", "steam").id) {
-        const PC = msg.guild.roles.find("name","PC")
-        var realuser = reaction.message.guild.member(user)
-        //user.addRole(user.guild.roles.add("name", "Steam"));
-        realuser.addRole(PC);
+    if(msg.channel.id === "431147353232244746") {
+        //if(reaction.emoji.name === client.emojis.get("431581850457079819")) {
+        //client.emojis.get("436916081957797889")
+        if (reaction.emoji.id === reaction.message.guild.emojis.find("name", "steam").id) {
+            const PC = msg.guild.roles.find("name","PC")
+            var realuser = reaction.message.guild.member(user)
+            //user.addRole(user.guild.roles.add("name", "Steam"));
+            realuser.addRole(PC);
+        }
     }
 });
 
