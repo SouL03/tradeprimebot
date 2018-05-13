db3bdf14a7ab24c9169f12caed1907184b7ee7c0
SouL03 <34333991+SouL03@users.noreply.github.com>
2018-05-14 00:35:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -77,7 +77,7 @@ client.on('messageReactionAdd', (reaction, user) => {
     	
    const msg = reaction.message
 
-   msg.channel.fetchMessage('445348783417458723').then(r => {	
+   msg.channel.fetchMessage('445353162015113228').then(r => {	
 	//if(msg.channel.id === '431147353232244746'){
 		if(reaction.emoji.id === reaction.message.guild.emojis.find("name", "steam").id) {
 		  const PC = msg.guild.roles.find('name','PC')
