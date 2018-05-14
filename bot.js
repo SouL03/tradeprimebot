3cc5808ef58f694b7c199c8960f7f75227b3937d
SouL03 <34333991+SouL03@users.noreply.github.com>
2018-05-14 04:12:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -84,8 +84,24 @@ client.on('messageReactionAdd', (reaction, user) => {
 		  var realuser = reaction.message.guild.member(user)
 		  realuser.addRole(PC);    
 		}
+
 	}
    })
+	
+});
+
+client.on('messageReactionRemove', (reaction, user) => {
+    
+   const msg = reaction.message
+
+    if(msg.channel.id === "431147353232244746") {
+
+        if(reaction.emoji.id === reaction.message.guild.emojis.find("name", "steam").id) {
+            const PC = msg.guild.roles.find('name','PC')
+            var realuser = reaction.message.guild.member(user)
+            realuser.removeRole(PC);    
+        }
+    }
 	
 });
     
